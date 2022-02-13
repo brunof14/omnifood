15a9c45240d66aa9489af69f778ab7970714b759
Bruno Felipe <oliveiraf.120@gmail.com>
2022-02-13 02:09:12 -0400

    add header sticky

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
 const buttonNavMobileEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+const sectionHeroEl = document.querySelector(".section-hero");
 
 function toggleMenuMobile() {
   headerEl.classList.toggle("nav-open");
 }
 
+function stickHeaderIntersectionCallback(entries) {
+  const [firstEntry] = entries;
+
+  if (firstEntry.isIntersecting) {
+    document.body.classList.remove("sticky");
+    return;
+  }
+
+  document.body.classList.add("sticky");
+}
+
+const interObs = new IntersectionObserver(stickHeaderIntersectionCallback, {
+  rootMargin: "-64px",
+});
+
 headerEl.addEventListener("click", toggleMenuMobile);
+interObs.observe(sectionHeroEl);
